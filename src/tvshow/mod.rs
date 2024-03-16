ef4a766376a5ed86f9e594a49549f3891d00a719
AGMBK <109393672+agmbk@users.noreply.github.com>
2024-03-16 02:08:05 +0100

    feat(tvshow): content ratings
    Implements the endpoints
    https://developer.themoviedb.org/reference/tv-series-content-ratings

--- src/tvshow/mod.rs
+++ src/tvshow/mod.rs
@@ -1,3 +1,9 @@
+use crate::common::country::Country;
+use crate::common::language::Language;
+use crate::company::CompanyShort;
+use crate::genre::Genre;
+use crate::people::PersonShort;
+
 #[cfg(feature = "commands")]
 pub mod details;
 #[cfg(feature = "commands")]
@@ -12,15 +18,11 @@ pub mod search;
 pub mod similar;
 #[cfg(feature = "commands")]
 pub mod watch_providers;
+#[cfg(feature = "commands")]
+pub mod content_rating;
 
 pub mod episode;
 pub mod season;
-
-use crate::common::country::Country;
-use crate::common::language::Language;
-use crate::company::CompanyShort;
-use crate::genre::Genre;
-use crate::people::PersonShort;
 
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 pub struct TVShowBase {
@@ -31,7 +33,7 @@ pub struct TVShowBase {
     pub origin_country: Vec<String>,
     #[serde(default)]
     pub overview: Option<String>,
-    #[serde(with = "crate::util::empty_date")]
+    #[serde(deserialize_with = "crate::util::empty_string::deserialize")]
     pub first_air_date: Option<chrono::NaiveDate>,
     #[serde(default)]
     pub poster_path: Option<String>,
@@ -53,7 +55,6 @@ pub struct TVShowShort {
 
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 pub struct EpisodeShort {
-    #[serde(with = "crate::util::date")]
     pub air_date: chrono::NaiveDate,
     pub episode_number: u64,
     pub id: u64,
@@ -78,7 +79,7 @@ pub struct Episode {
 
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 pub struct SeasonBase {
-    #[serde(with = "crate::util::optional_date")]
+    #[serde(deserialize_with = "crate::util::empty_string::deserialize")]
     pub air_date: Option<chrono::NaiveDate>,
     pub id: u64,
     pub name: String,
@@ -108,14 +109,13 @@ pub struct Season {
 pub struct TVShow {
     #[serde(flatten)]
     pub inner: TVShowBase,
-    //
     pub created_by: Vec<PersonShort>,
     pub episode_run_time: Vec<u64>,
     pub genres: Vec<Genre>,
     pub homepage: String,
     pub in_production: bool,
     pub languages: Vec<String>,
-    #[serde(with = "crate::util::optional_date")]
+    #[serde(deserialize_with = "crate::util::empty_string::deserialize")]
     pub last_air_date: Option<chrono::NaiveDate>,
     pub last_episode_to_air: Option<EpisodeShort>,
     pub next_episode_to_air: Option<EpisodeShort>,
